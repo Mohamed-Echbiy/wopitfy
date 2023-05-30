b704c166a0b07206957d812af0355dc487b90669
Mohamed-Echbiy <echbiymed@gmail.com>
2023-05-30 15:42:13 +0100

    adding basic layout for sidebAR

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,11 +2,29 @@
 
 import React from "react";
 
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
 function Library() {
   const onClick = () => {
     //handel upload later
   };
-  return <div className="flex flex-col">Library</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-center px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="text-prim cursor-pointer hover:text-white transition-all"
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 px-3 mt-4">List Of Songs</div>
+    </div>
+  );
 }
 
 export default Library;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prim: "text-neutral-400",
+      },
+    },
   },
   plugins: [],
 };
