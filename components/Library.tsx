1bd312dc198c278e30e7d3edeaae3cbe3d277540
Mohamed-Echbiy <echbiymed@gmail.com>
2023-05-30 15:33:23 +0100

    start the woptify project

---
+++ components/Library.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+function Library() {
+  const onClick = () => {
+    //handel upload later
+  };
+  return <div className="flex flex-col">Library</div>;
+}
+
+export default Library;
